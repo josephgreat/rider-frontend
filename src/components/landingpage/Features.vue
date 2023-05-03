fe39e650b8f39b6615b66bef21417cd47e7c508b
josephgreat <josephgreat2003@gmail.com>
2023-05-04 00:57:26 +0100

    Still working on feaures

---
+++ src/components/landingpage/Features.vue
@@ -0,0 +1,36 @@
+<script></script>
+<template>
+    <div class="max-w-[72rem] mx-auto px-3 mb-12 sm:px-10 py-10">
+        <div class="md:grid flex flex-col  md:grid-cols-[1fr_1fr_2fr] gap-5 mb-5 ">
+            <div class="bg-gray-200 rounded-3xl px-6 py-10 ">
+                <h4 class="font-semibold text-[2rem] leading-[1.2] mb-4">Real Time Tracking</h4>
+                <p>you can track your package anytime and anywhere</p>
+            </div>
+            <div class="bg-gray-200 rounded-3xl px-6 py-10">
+                <h4 class="font-semibold text-[2rem] leading-[1.2] mb-4">Packaging</h4>
+                <p>we use quality packaging for the safety of your goods</p>
+            </div>
+            <div class="bg-gray-200  rounded-3xl px-6 py-10">
+                <h4 class="font-semibold text-[2rem] leading-[1.2] mb-4">Mobile App Tracker</h4>
+                <p>you can check your package using mobile. This makes it possible for you to find out where your package is anytime and anywhere!</p>
+            </div>
+        </div>
+        <div class="grid grid-cols-[2fr_1fr_1fr] gap-5">
+            <div class="bg-gray-200 rounded-3xl px-6 py-10">
+                <h4 class="font-semibold text-[2rem] leading-[1.2] mb-4">Insurance Services</h4>
+                <p>we will provide insurance if your item is damaged on transfer. So don't worry if yor item is damaged because we will take care of it</p>
+            </div>
+            <div class="bg-gray-200 rounded-3xl px-6 py-10">
+                <h4 class="font-semibold text-[2rem] leading-[1.2] mb-4">Pick up Delivery</h4>
+                <p>we will pick up your package at home. Wait there, we'll be there!</p>
+            </div>
+            <div class="bg-gray-200 rounded-3xl px-6 py-10">
+                <h4 class="font-semibold text-[2rem] leading-[1.2] mb-4">24 Hours</h4>
+                <p>we will send your package non-stop so that it arrives quickly</p>
+            </div>
+        </div>
+        
+    </div>
+</template>
+
+<style></style>
