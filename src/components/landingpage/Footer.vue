e081d2a89ada54e87e9bd1a8601d07a74c351fe3
edu <edunwa302@gmail.com>
2023-05-06 14:15:52 +0100

    fix some text and color different from design image

--- src/components/landingpage/Footer.vue
+++ src/components/landingpage/Footer.vue
@@ -1,87 +1,89 @@
-<script></script>
+<script setup>
+import {ref} from 'vue'
+let currentYear = ref('')
+currentYear =  new Date().getFullYear()
+</script>
 <template>
     <footer class="bg-[#262626] rounded-t-[3rem]">
         <div class="max-w-[72rem] mx-auto px-3  sm:px-10">
             <div
                 class="flex flex-col sm:flex-row py-10 md:gap-10 gap-2 mb-10 border-b border-[#464646] sm:items-center sm:justify-between font-semibold">
-                <h3 class="order-2 sm:order-1 md:text-[3.2rem] text-[2rem] ">
+                <h3 class="order-2 sm:order-1 md:text-[3rem] text-[2rem] text-[#ffffffb3]">
                     <span class="primary">Let's</span>
                     start with us in
                     <br/>
                     <span class="primary">shipping</span>
                     your goods!
                 </h3>
-                <div class="w-fit bg-[#464646] order-1 sm:order-2 rounded-[2rem] h-fit px-10 py-5">
-                    <i class="fa fa-level-up rotate-45 primary text-[3rem] md:text-[5rem]"></i>
+                <div class="w-fit bg-[#464646] order-1 sm:order-2 rounded-[2rem] h-fit px-8 py-4 hover:to-[#2f4750] hover:border-[1px] border-cyan-300">
+                    <i class="fa fa-level-up rotate-45 primary text-[3rem] md:text-[4rem]"></i>
                 </div>
             </div>
             <div>
                 <div class="flex flex-col md:flex-row justify-between gap-2 md:gap-10">
                     <div class="mb-4 md:w-1/3">
                         <img
-                            class="sm:w-2/6 w-1/2 mb-4"
+                            class="sm:w-2/6 w-1/2 mb-4 opacity-60"
                             src="../../assets/logo/vector/default-monochrome-white.svg"
                             alt="Riderjet logo"/>
-                        <p class="opacity-50 mb-4">Trust your package to us, we have been trusted by the
+                        <p class="opacity-50 mb-4 text-[#ffffffb3]">Trust your package to us, we have been trusted by the
                             whole world, your package must be safe!</p>
-                        <p class="opacity-50">helloriderjet@gmail.com</p>
+                        <p class="opacity-50">info@riderjet.com</p>
                     </div>
                     <ul class="flex flex-wrap sm:flex-nowrap md:justify-between md:gap-10 md:w-1/2">
                         <li class="w-1/2 sm:w-1/3 mb-4">
-                            <ul>
-                                <li class="mb-2">
+                            <ul class="text-[#ffffffb3]">
+                                <li class="mb-2 hover:text-[#4cbbe5]">
                                     <a href="#">HOME</a>
                                 </li>
-                                <li class="mb-2">
+                                <li class="mb-2 hover:text-[#4cbbe5]">
                                     <a href="#">ABOUT</a>
                                 </li>
-                                <li class="mb-2">
-                                    <a href="#">SERVICES</a>
+                                <li class="mb-2 hover:text-[#4cbbe5]">
+                                    <a href="#">CAREERS</a>
                                 </li>
-                                <li class="mb-2">
+                                <li class="mb-2 hover:text-[#4cbbe5]">
                                     <a href="#">OFFICE</a>
                                 </li>
                             </ul>
                         </li>
                         <li class="w-1/2 sm:w-1/3 mb-4">
-                            <ul>
-                                <li class="mb-2">
+                            <ul class="text-[#ffffffb3]">
+                                <li class="mb-2 hover:text-[#4cbbe5]">
                                     <a href="#" class="fa fa-instagram" aria-label="INSTAGRAM"></a>
                                 </li>
-                                <li class="mb-2">
+                                <li class="mb-2 hover:text-[#4cbbe5]">
                                     <a href="#" class="fa fa-facebook" aria-label="facebook"></a>
                                 </li>
-                                <li class="mb-2">
+                                <li class="mb-2 hover:text-[#4cbbe5]">
                                     <a href="#" class="fa fa-twitter" aria-label="Twitter"></a>
                                 </li>
-                                <li class="mb-2">
+                                <li class="mb-2 hover:text-[#4cbbe5]">
                                     <a href="#" class="fa fa-envelope" aria-label="Email"></a>
                                 </li>
                             </ul>
                         </li>
-                        <li class="w-full sm:w-1/3 mb-4">
+                        <li class="w-full sm:w-1/3 mb-4 text-[#ffffffb3]">
                             <ul>
-                                <li class="mb-2">
+                                <li class="mb-2 hover:text-[#4cbbe5]">
                                     <a href="#">PRIVACY POLICY</a>
                                 </li>
-                                <li class="mb-2">
-                                    <a href="#">FArrr33rQ</a>
+                                <li class="mb-2 hover:text-[#4cbbe5]">
+                                    <a href="#">FAQ</a>
                                 </li>
-                                <li class="mb-2">
+                                <li class="mb-2 hover:text-[#4cbbe5]">
                                     <a href="#">TERMS & CONDITIONS</a>
                                 </li>
                             </ul>
                         </li>
                     </ul>
                 </div>
+                <p class="text-xs text-center tracking-widest text-[#ffffffb3] py-2">Copyright © {{ currentYear }} RiderJet</p>
             </div>
         </div>
     </footer>
 </template>
 
 <style scoped>
-    * {
-        color: #fff;
-    }
 
 </style>
--- src/components/landingpage/Services.vue
+++ src/components/landingpage/Services.vue
@@ -1,24 +1,24 @@
-<script></script>
+<script setup></script>
 <template>
     <div class="max-w-[72rem] mx-auto px-3 mb-12 sm:px-10 pb-10">
         <div class="flex flex-col gap-2 md:gap-10 md:flex-row justify-between md:items-center">
-            <h3 class="md:text-[3.2rem] text-[2.5rem] font-semibold">Delivery of your
+            <h3 class="dark:text-[#ffffffb3] text-[#333333] md:text-[3.2rem] text-[2.5rem]">Delivery of your
                 <span class="primary">Package!</span>
             </h3>
-            <p>We provide all access to simplify and carry out your package delivery.
+            <p class="dark:text-[#ffffffb3] text-[#333333]">We provide all access to simplify and carry out your package delivery.
                 Riderjet will give you the best!</p>
         </div>
         <ul class="grid md:grid-cols-[1fr_1fr_1fr] gap-5 mt-10">
-            <li class="text-[2rem] font-semibold bg-gradient px-6 py-10 rounded-[3rem] mb-4">
-                <div class="bg-white px-5 py-2 rounded-full inline-block mb-24"><i class="fa fa-globe primary"></i></div>
-                <h4 class="text-white">Worldwide Shipping</h4>
+            <li class="text-[2rem] dark:bg-[#323232] dark:text-[#ffffffb3] text-[#333333] bg-gray-200 font-semibold px-6 py-10 rounded-[3rem] mb-4 hover:text-[#ffffffb3] hover:bg-gradient-to-l hover:from-[#2f4750] hover:via-[#4cbce5] hover:to-[#2f4750] hover:border-[1px] border-cyan-300">
+                <div class="px-5 py-2 rounded-full inline-block mb-24"><i class="fa fa-globe text-[#4cbbe5]"></i></div>
+                <h4>Worldwide Shipping</h4>
             </li>
-            <li class="text-[2rem] font-semibold dark:bg-[#323232] bg-gray-200 px-6 py-10 rounded-[3rem] mb-4">
-                <div class="bg-white px-4 py-2 rounded-full inline-block mb-24"><i class="fa fa-car dark:text-[#262626]"></i></div>
+            <li class="text-[2rem] font-semibold dark:bg-[#323232] dark:text-[#ffffffb3] text-[#333333] bg-gray-200 px-6 py-10 rounded-[3rem] mb-4 hover:text-[#ffffffb3] hover:bg-gradient-to-l hover:from-[#2f4750] hover:via-[#4cbce5] hover:to-[#2f4750] hover:border-[1px] border-cyan-300">
+                <div class="px-4 py-2 rounded-full inline-block mb-24"><i class="fa fa-car text-[#4cbbe5]"></i></div>
                 <h4>Ground Shipping</h4>
             </li>
-            <li class="text-[2rem] font-semibold dark:bg-[#323232] bg-gray-200 px-6 py-10 rounded-[3rem] mb-4">
-                <div class="bg-white px-4 py-2 rounded-full inline-block mb-24"><i class="fa fa-ship dark:text-[#262626]"></i></div>
+            <li class="text-[2rem] font-semibold dark:bg-[#323232] dark:text-[#ffffffb3] text-[#333333] bg-gray-200 px-6 py-10 rounded-[3rem] mb-4 hover:text-[#ffffffb3] hover:bg-gradient-to-l hover:from-[#2f4750] hover:via-[#4cbce5] hover:to-[#2f4750] hover:border-[1px] border-cyan-300">
+                <div class="px-4 py-2 rounded-full inline-block mb-24"><i class="fa fa-ship text-[#4cbbe5]"></i></div>
                 <h4>Sea Shipping</h4>
             </li>
         </ul>
